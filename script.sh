46895f918e0338bb49b8cd2c829fd812ad995d47
Ahmed Nabil Zin EL-Din <a_nabil2k@hotmail.com>
2024-07-31 19:18:59 +0300

    dragon

--- script.sh
+++ script.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
-echo "Hello World"
-echo "welcom"
+sudo apt-get install cowsay -y
+cowsay -f dragon " Run for cover, iam a Dragon .....RAWR">> dragon.txt
+grep -i "dragon" dragon.txt
+cat dragon.txt
+ls -ltra
